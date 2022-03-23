801223822211c1089fbf4c8b66e8e322d617aff5
alanhakhyeonsong <songs4805@naver.com>
2022-03-23 21:07:46 +0900

    BOJ 10026 python

--- codes/BOJ/DFS, BFS/10026.py
+++ codes/BOJ/DFS, BFS/10026.py
@@ -1,8 +1,8 @@
 # BOJ 10026번: 적록색약
 from collections import deque
 
-def bfs(x,y):
-    q.append((x,y))
+def bfs(x, y):
+    q.append((x, y))
     dx = [0, 0, -1, 1]
     dy = [-1, 1, 0, 0]
     visited[x][y] = True
@@ -11,12 +11,12 @@ def bfs(x,y):
         for d in range(4):
             nx = x + dx[d]
             ny = y + dy[d]
-            if 0 <= nx < n and 0 <= ny < n and a[nx][ny] == a[x][y] and not visited[nx][ny]:
-                visited[nx][ny] = 1
+            if 0 <= nx < n and 0 <= ny < n and grid[nx][ny] == grid[x][y] and not visited[nx][ny]:
+                visited[nx][ny] = True
                 q.append((nx, ny))
 
 n = int(input())
-a = [list(input()) for _ in range(n)]
+grid = [list(input()) for _ in range(n)]
 q = deque()
 
 visited = [[False] * n for _ in range(n)]
@@ -29,8 +29,8 @@ for i in range(n):
 
 for i in range(n):
     for j in range(n):
-        if a[i][j] == 'G':
-            a[i][j] = 'R'
+        if grid[i][j] == 'G':
+            grid[i][j] = 'R'
 
 visited = [[False] * n for _ in range(n)]
 blind = 0
