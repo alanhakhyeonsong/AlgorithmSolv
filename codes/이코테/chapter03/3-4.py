95e64709933d6ce2cee76224d468687e9606d1c4
alanhakhyeonsong <songs4805@naver.com>
2022-07-13 22:33:26 +0900

    chapter03

---
+++ codes/이코테/chapter03/3-4.py
@@ -0,0 +1,13 @@
+from sys import stdin
+
+n, k = map(int, stdin.readline().split())
+count = 0
+while n != 1:
+    if n % k == 0:
+        n //= k
+        count += 1
+    else:
+        n -= 1
+        count += 1
+
+print(count)
