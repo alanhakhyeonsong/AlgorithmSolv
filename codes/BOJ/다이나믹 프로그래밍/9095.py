ffcc67262f45fc9743cdb10f316d79620935aa38
alanhakhyeonsong <songs4805@naver.com>
2022-07-20 18:02:57 +0900

    BOJ 9095

---
+++ codes/BOJ/다이나믹 프로그래밍/9095.py
@@ -0,0 +1,14 @@
+# BOJ 9095번: 1, 2, 3 더하기
+
+n = int(input())
+d = [0] * 11
+d[1] = 1
+d[2] = 2
+d[3] = 4
+
+for i in range(4, 11):
+    d[i] = d[i-3] + d[i-2] + d[i-1]
+
+for i in range(n):
+    x = int(input())
+    print(d[x])
